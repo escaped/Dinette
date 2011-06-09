5c2db32ea5ead77f78abf84fbc70917b7e47bbb6
Jonas <jvp@jonasundderwolf.de>
2011-06-09 11:56:17 +0200

    Append slashes to some URLs where they were missing

--- dinette/urls.py
+++ dinette/urls.py
@@ -1,5 +1,4 @@
 from django.conf.urls.defaults import *
-from django.contrib.syndication.views import feed
 
 from dinette.views import LatestTopicsByCategory,LatestRepliesOfTopic
 
@@ -27,7 +26,7 @@ urlpatterns += patterns('dinette.views',
     url(r'^search/$','search',name='dinette_search'),
     
     # user profile page
-    url(r'^users/(?P<slug>[\w-]+)$', 'user_profile', name='dinette_user_profile'),
+    url(r'^users/(?P<slug>[\w-]+)/$', 'user_profile', name='dinette_user_profile'),
 
     # subscribe to digest
     url(r'^digest/subscribe/$', 'subscribeDigest', name='dinette_subscribe_to_digest'),
@@ -47,8 +46,8 @@ urlpatterns += patterns('dinette.views',
     url(r'^moderate/topic/(?P<topic_id>\d+)/hide/$','moderate_topic', {'action':'hide'}, name='dinette_moderate_hide'),
     
     # post actions, permitted to OP and mods
-    url(r'^delete/reply/(?P<reply_id>\d+)$','deleteReply', name='dinette_deletereply'),
-    url(r'^edit/reply/(?P<reply_id>\d+)$','editReply', name='dinette_editreply'),
+    url(r'^delete/reply/(?P<reply_id>\d+)/$','deleteReply', name='dinette_deletereply'),
+    url(r'^edit/reply/(?P<reply_id>\d+)/$','editReply', name='dinette_editreply'),
 
     # subscribe to topic
     url(r'^subscribe/topic/(?P<topic_id>\d+)', 'subscribeTopic', name='dinette_subscribe_to_topic'),
